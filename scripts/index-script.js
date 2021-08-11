5490f4a6a5abfef1e3eea702f573cae226514506
knopkalit <80261754+knopkalit@users.noreply.github.com>
2021-08-11 18:13:15 +0300

    Add files via upload

--- scripts/index-script.js
+++ scripts/index-script.js
@@ -47,15 +47,15 @@ for (let i = 0; i < productCard.length; i++) {
         for (let j = 0; j < productCard.length; j++) {
             if (i != j) {
                 //productCard[j].style.opacity = '0.5';
-                productCard[j].style.transition = '0.3s';
+                //productCard[j].style.transition = '0.3s';
             }
         }
     });
     productCard[i].addEventListener('mouseleave', function() {
-        sizeLine[i].style.top = 'calc(100% - 15px)';
+        sizeLine[i].style.top = 'calc(100% - 10px)';
         for (let j = 0; j < productCard.length; j++) {
                 //productCard[j].style.opacity = '1';
-                productCard[j].style.transition = '0.3s';
+                //productCard[j].style.transition = '0.3s';
         }
     });
 }
