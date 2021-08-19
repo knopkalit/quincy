12474254811480c3d5d58c9c540f3385f0e75966
knopkalit <80261754+knopkalit@users.noreply.github.com>
2021-08-19 15:27:34 +0300

    Add files via upload

--- scripts/product-script.js
+++ scripts/product-script.js
@@ -1,5 +1,8 @@
-let sizeBlock = document.querySelector('.sizeBlock'),
+let sizeBlock = document.querySelector('.sizeBlock-inner'),
 sizeListItem = document.querySelectorAll('.sizeBlock__selectList');
+if (!document.querySelector('.sizeBlock__list').classList.contains('undisplay')) {
+    document.querySelector('.sizeBlock__arrow').classList.remove('arrow-rotate');
+}
 
 sizeBlock.addEventListener('click', function(event) {
     document.querySelector('.sizeBlock__arrow').classList.toggle('arrow-rotate');
@@ -23,27 +26,51 @@ for (let i = 0; i < sizeListItem.length; i++) {
         document.querySelector('.sizeList-mobile').classList.remove('active--size');
         document.querySelector('.pageOverlay').classList.remove('visible');
         document.querySelector('.sizeBlock__selected').innerHTML = sizeListItem[i].innerHTML;
+        document.querySelector('.sizeBlock__arrow').classList.remove('arrow-rotate');
+        document.querySelector('.sizeBlock__list').classList.add('undisplay');
+        document.body.classList.remove('lock-mobile');
+        document.querySelector('.settings__select').classList.remove('fe-45-1');
+        document.querySelector('.settings__select').classList.remove('fe-45-2');
+        document.querySelector('.settings__color').classList.remove('fe-45-1');
+        document.querySelector('.settings__color').classList.remove('fe-45-2');
+        document.querySelector('.titleManagement').classList.remove('fe-45-1');
+        document.querySelector('.titleManagement').classList.remove('fe-45-2');
+        document.querySelector('.sizeBlock-inner').classList.remove('fe-45-1');
     });
 }
 document.querySelector('.sizeBlock-inner').addEventListener('click', function() {
     document.querySelector('.sizeList-mobile').classList.add('active--size');
     document.querySelector('.pageOverlay').classList.add('visible');
+    if (document.querySelector('.sizeList-mobile').classList.contains('active--size')) {
+        document.body.classList.add('lock-mobile');
+    } else {
+        document.body.classList.remove('lock-mobile');
+    }
 });
 document.querySelector('.sizeList-mobile__close').addEventListener('click', function() {
     document.querySelector('.sizeBlock__arrow').classList.remove('arrow-rotate');
     document.querySelector('.sizeList-mobile').classList.remove('active--size');
     document.querySelector('.pageOverlay').classList.remove('visible');
+    document.body.classList.remove('lock-mobile');
 });
 document.querySelector('.pageOverlay').addEventListener('click', function(event) {
     if (!event.target.closest('.sizeList-mobile')) {
         document.querySelector('.sizeList-mobile').classList.remove('active--size');
         document.querySelector('.pageOverlay').classList.remove('visible');
+        document.body.classList.remove('lock-mobile');
+        document.querySelector('.sizeBlock__arrow').classList.remove('arrow-rotate');
     }
 });
 document.addEventListener('keydown', function(event) {
     if (event.keyCode === 27) {
         document.querySelector('.sizeList-mobile').classList.remove('active--size');
         document.querySelector('.pageOverlay').classList.remove('visible');
+        document.body.classList.remove('lock-mobile');
+        if (document.querySelector('.sizeBlock__arrow').classList.contains('arrow-rotate')) {
+            document.querySelector('.sizeBlock__arrow').classList.remove('arrow-rotate');
+        } else {
+            document.querySelector('.sizeBlock__arrow').classList.add('arrow-rotate');
+        }
     }
 });
 
